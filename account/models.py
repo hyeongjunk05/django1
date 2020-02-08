b69aca25ea389d224cbe013b69df1bbb4fa0c681
Hyeongjun Kim <hyeongjunkim@Hyeongjunui-MacBookAir.local>
2020-02-08 21:24:21 +0900

    장고

---
+++ account/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Account(models.Model):
+
+    
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'accountss'
---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views      import SignInView, SignUpView
+urlpatterns = [
+    path('/sign-up', SignUpView.as_view()),
+    path('', SignUpView.as_view()),
+    path('/sign-in', SignInView.as_view()),
+    # path('/comment', AccountComment.as_view()),
+]
